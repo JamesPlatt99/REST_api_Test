560654bff6891b1a0d1d15483270a01ae1fdf917
James Platt <James.Platt@Neptune.com>
2018-06-10 10:24:00 +0100

    Renamed SalesOrderDetailsController to SalesOrdersController

--- Controllers/SalesOrdersController.cs
+++ Controllers/SalesOrdersController.cs
@@ -6,14 +6,14 @@ using System.Linq;
 namespace MyApi.Controllers
 {
     [Route("api/[controller]")]
-    public class SalesOrderDetailsController : Controller
+    public class SalesOrdersController : Controller
     {
 #region Properties
         private AdventureWorks2016CTP3Context _context;
 #endregion
 
 #region Constructor
-        public SalesOrderDetailsController(Context.AdventureWorks2016CTP3Context context)
+        public SalesOrdersController(Context.AdventureWorks2016CTP3Context context)
         {
             _context = context;
         }
@@ -21,14 +21,14 @@ namespace MyApi.Controllers
 
 #region API Calls
         [HttpGet("{CustomerID}")]
-        public IActionResult SalesOrderDetails(int customerID)
+        public IActionResult SalesOrders(int customerID)
         {
-            return new JsonResult(GetSalesOrderDetail(customerID));
+            return new JsonResult(GetSalesOrders(customerID));
         }
 #endregion
 
 #region  Helper Methods
-        private IEnumerable<Models.SalesOrderDTO> GetSalesOrderDetail(int customerID)
+        private IEnumerable<Models.SalesOrderDTO> GetSalesOrders(int customerID)
         {
             var query = from soh in _context.SalesOrderHeader
                         join sod in _context.SalesOrderDetail on soh.SalesOrderId equals sod.SalesOrderId
